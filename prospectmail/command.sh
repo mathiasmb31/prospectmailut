5c2b4cd3b155fa334806839d542167a9f4c66e06
Mathias <mathias.brethous@outlook.fr>
2026-02-26 19:20:46 +0100

    code cleanup

--- prospectmail/command.sh
+++ prospectmail/command.sh
@@ -6,36 +6,18 @@ echo $SNAP
 export WD=${PWD}
 echo "Going to launch"
 echo "GRID UNIT PX"$GRID_UNIT_PX
-#export QT_QUICK_CONTROLS_STYLE=Suru
-#export QT_EXCLUDE_GENERIC_BEARER=1
-#export GDK_DEBUG=gl-gles
-#export SNAP_REEXEC=0
-#export QT_QPA_PLATFORM="ubuntumirclient;wayland-egl;xcb"
-#export LESSCLOSE=/usr/bin/lesspipe %s %s
-#export XDG_SESSION_CLASS=user
-#export GDK_GL=gles
-#export TERM=xterm-256color
-#export LESSOPEN=| /usr/bin/lesspipe %s
-#export USER=phablet
-#export SDL_VIDEODRIVER=wayland
 export DISPLAY=:0.0
-#export SHLVL=1
-#export XDG_SESSION_ID=c119
 export QT_FILE_SELECTORS=ubuntu-touch
-#export XDG_RUNTIME_DIR=/run/user/32011
-#export XDG_DATA_DIRS=/usr/local/share:/usr/share:/var/lib/snapd/desktop
-#PATH=${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-#export QTWEBENGINE_CHROMIUM_FLAGS=--enable-gpu-rasterization --enable-accelerated-video-decode --enable-features=MojoVideoDecoder
-#export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/32011/bus
+
 dpioptions="--high-dpi-support=1 --force-device-scale-factor=1 --grid-unit-px=1"
 sandboxoptions="--no-sandbox"
-#gpuoptions="--use-gl=egl --enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist --enable-features=UseSkiaRenderer,VaapiVideoDecoder --disable-frame-rate-limit --disable-gpu-vsync --enable-oop-rasterization"
 gpuoptions="--use-gl=egl --disable-dev-shm-usage"
 echo "------------------------------------------------------------------"
-#exec "$SNAP/desktop-init.sh" "$SNAP/desktop-common.sh" "$SNAP/desktop-gnome-specific.sh" "$SNAP/app/prospect-mail $dpioptions $sandboxoptions $gpuoptions "
-echo $$ >> /home/phablet/.config/prospectmail.mathias/data/__prospect.pid
-( utils/shortsleep.sh;utils/auto_zoom.sh )&
+echo $$ >>/home/phablet/.config/prospectmail.mathias/data/__prospect.pid
+(
+          utils/shortsleep.sh
+          utils/auto_zoom.sh
+) &
 export PATH=$PWD/bin:$PATH
 echo $PATH
-$SNAP/app/prospect-mail $dpioptions $sandboxoptions $gpuoptions 
-
+$SNAP/app/prospect-mail $dpioptions $sandboxoptions $gpuoptions
--- prospectmail/get.sh
+++ prospectmail/get.sh
@@ -1,9 +1,10 @@
 SNAP_FILE="prospect-mail.snap"
 cd ${BUILD_DIR}
-DOWNLOAD_URL=$(curl -s https://api.snapcraft.io/v2/snaps/info/prospect-mail -H "Snap-Device-Series: 16" -H "Snap-Architecture: arm64" | jq -r '.["channel-map"][] 
+DOWNLOAD_URL=$(
+          curl -s https://api.snapcraft.io/v2/snaps/info/prospect-mail -H "Snap-Device-Series: 16" -H "Snap-Architecture: arm64" | jq -r '.["channel-map"][] 
             | select(.channel.architecture=="arm64" and .channel.name=="stable") 
             | .download.url'
-            )
+)
 if ! test -e "$ROOT/$SNAP_FILE"; then
-	    curl -L -o "$ROOT/$SNAP_FILE" "$DOWNLOAD_URL"
+          curl -L -o "$ROOT/$SNAP_FILE" "$DOWNLOAD_URL"
 fi
