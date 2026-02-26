fc71ea8b8b8ebab087f337544c3556f9b11c39a1
Mathias <mathias.brethous@outlook.fr>
2026-02-26 15:50:58 +0100

    Add Feature open url

--- prospectmail/utils/xdg-open/xdg-open-test.cpp
+++ prospectmail/utils/xdg-open/xdg-open-test.cpp
@@ -7,17 +7,17 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    qInfo() << " Local xdg-open" ;
     qWarning() << "Usage: xdg-open-test <url ou fichier>will try to open" << argv[1];
-
+	QString input="";
     // Vérifie qu'un argument (URL ou chemin) a été passé
     if (argc < 2) {
         qWarning() << "Usage: xdg-open-test <url ou fichier>";
-        return 1;
-    }
-
-    QString input = QString::fromLocal8Bit(argv[1]);
-    QUrl url = QUrl::fromUserInput(input);
-
+      input="http://www.google.fr";  
+    } else {
+		input = QString::fromLocal8Bit(argv[1]);
+	}
+	QUrl url = QUrl::fromUserInput(input);
     if (!url.isValid()) {
         qWarning() << "URL invalide:" << input;
         return 2;
