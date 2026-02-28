0b496fcd62dd97511c95bd33c7814c3bf5fb13ad
Mathias <mathias.brethous@outlook.fr>
2026-02-28 12:36:28 +0100

    Feature added: echo"nb to zoom" > .config/prospectmail.mathias/zoomtodo

---
+++ prospectmail/utils/rm.sh
@@ -0,0 +1,21 @@
+#!/bin/python3
+import os
+import sys
+import shutil
+
+# Vérifier qu'un argument a été fourni
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <file> ")
+    exit(1)
+
+# Récupérer le chemin dans le premier argument
+
+file = sys.argv[1]
+
+
+# If file exists, delete it.
+if os.path.isfile(file):
+    os.remove(file)
+else:
+    # If it fails, inform the user.
+    print("Error: %s file not found" % file)
