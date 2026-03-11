5df513e894a063386e8e5d5956c36898dca36cb1
Mathias <mathias.brethous@outlook.fr>
2026-03-11 10:00:55 +0100

    Script to reset prospectmail config

---
+++ prospectmail/utils/reset_zoom.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "..launch this script in /opt/opt/click.ubuntu.com/prospectmail.mathias/current/utils...."
+rm -rf  /home/phablet/.config/prospectmail.mathias/FIRSTINSTALL
+
+./rm.sh /home/phablet/.config/prospectmail.mathias/prospect-mail/Preferences
+../bin/notify "Reset done...launch now prospectmail"
