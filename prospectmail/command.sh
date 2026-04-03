3c5518e798a4002cdb7dbe5bec3000e70b5274ca
Mathias <mathias.brethous@outlook.fr>
2026-04-03 18:35:39 +0200

    adapt to latest nodejs

--- prospectmail/command.sh
+++ prospectmail/command.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -ax
+export WD=`pwd`
+echo $WD
+sleep 5
+
 setopt LOCAL_OPTIONS allexport
 SHELL=/bin/bash
 WINDOWID=0
@@ -36,30 +40,15 @@ HOME=/home/phablet
 LC_PAPER=fr_FR.UTF-8
 LANG=fr_FR.UTF-8
 QV4_ENABLE_JIT_CACHE=1
-LS_COLORS=rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=00:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.avif=01;35:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.webp=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:*~=00;90:*#=00;90:*.bak=00;90:*.crdownload=00;90:*.dpkg-dist=00;90:*.dpkg-new=00;90:*.dpkg-old=00;90:*.dpkg-tmp=00;90:*.old=00;90:*.orig=00;90:*.part=00;90:*.rej=00;90:*.rpmnew=00;90:*.rpmorig=00;90:*.rpmsave=00;90:*.swp=00;90:*.tmp=00;90:*.ucf-dist=00;90:*.ucf-new=00;90:*.ucf-old=00;90:
-MEMORY_PRESSURE_WATCH=/sys/fs/cgroup/user.slice/user-32011.slice/user@32011.service/app.slice/lomiri-app-launch--application-click--terminal.ubports_terminal_2.0.5--.service/memory.pressure
 QT_QUICK_CONTROLS_STYLE=Suru
 QT_EXCLUDE_GENERIC_BEARER=1
-XDG_SEAT_PATH=/org/freedesktop/DisplayManager/Seat0
-INVOCATION_ID=c15f4bf17fc64b89899c30b3808b51f2
-GDK_DEBUG=gl-gles
-MANAGERPID=24026
-SNAP_REEXEC=0
-DESKTOP_FILE_HINT=terminal.ubports_terminal_2.0.5
-APP_DESKTOP_FILE_PATH=/usr/share/click/preinstalled/.click/users/@all/terminal.ubports/lomiri-terminal-app.desktop
-QT_QPA_PLATFORM=ubuntumirclient;wayland-egl;xcb
 XDG_CACHE_HOME=/home/phablet/.config/prospectmail.mathias/data
 XDG_SESSION_CLASS=user
-GDK_GL=gles
 TERM=xterm
 LC_IDENTIFICATION=fr_FR.UTF-8
 LESSOPEN=| /usr/bin/lesspipe %s
 USER=phablet
-SDL_VIDEODRIVER=wayland
-MIR_SOCKET=/run/user/32011/mir_socket
-COLORFGBG=15;0
 export DISPLAY=:0.0
-SHLVL=1
 GSM_SKIP_SSH_AGENT_WORKAROUND=true
 LC_TELEPHONE=fr_FR.UTF-8
 export QT_IM_MODULE=maliit
@@ -68,18 +57,13 @@ QT_FILE_SELECTORS=ubuntu-touch
 PAPERSIZE=a4
 XDG_RUNTIME_DIR=/run/user/32011
 LC_TIME=fr_FR.UTF-8
-NATIVE_ORIENTATION=Portrait
-MIR_SERVER_NAME=session-1
-JOURNAL_STREAM=9:1230221
 XDG_DATA_DIRS=/home/phablet/.config/prospectmail.mathias/data
 GDMSESSION=ubuntu-touch
-QTWEBENGINE_CHROMIUM_FLAGS=--enable-gpu-rasterization --enable-accelerated-video-decode --enable-features=MojoVideoDecoder
 LC_NUMERIC=fr_FR.UTF-8
-QTWEBKIT_DPR=2.0
 trap 'printf "%3d: " "$LINENO"' DEBUG
 echo $SNAP
-sleep 2
-export WD=${PWD}
+echo $WD
+
 echo "Going to launch"
 echo "GRID UNIT PX"$GRID_UNIT_PX
 export QT_FILE_SELECTORS=ubuntu-touch
@@ -87,9 +71,21 @@ export QT_FILE_SELECTORS=ubuntu-touch
 echo "------------------------------------------------------------------"
 echo $$ >>/home/phablet/.config/prospectmail.mathias/data/__prospect.pid
 utils/auto_zoom.sh &
-export PATH=$PWD/bin:$PATH
+export PATH=$WD/bin:$PATH
 echo $PATH
 export XDG_CACHE_HOME=/home/phablet/.config/prospectmail.mathias/
 export TMPDIR=/home/phablet/.config/prospectmail.mathias/
-${WD}/bin/app/prospect-mail --no-sandbox --disable-gpu --ozone-platform=x11
+export SNAP=$WD/bin/
+export SNAP_DESKTOP_LAST_REVISION="3"
+export SNAP_REVISION="3"
+export GTK_IM_MODULE=Maliit
+export GTK_IM_MODULE_FILE=$WD/lib/aarch64-linux-gnu/gtk-3.0/3.0.0/immodules/immodules.cache
+export LD_LIBRARY_PATH=$WD/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
+export MALIIT_SOCKET_ADDRESS=unix:path=/run/user/32011/maliit
+export GTK_MODULES=gail:atk-bridge:maliitplatforminputcontextplugin
+export QT_IM_MODULE=maliit
+export XMODIFIERS=@im=Maliit
+echo "launch prospect" 
+echo "-----------------------------------------------------------------"
+bin/app/prospect-mail --no-sandbox --disable-gpu --ozone-platform=x11 
 echo "launched prospect"
